c88ae2c8ec1a6a137d5e7847bf24c71e2cf9e77e
Vakha <gvardee95@gmail.com>
2021-09-22 10:56:33 +0300

    created functions

---
+++ functions.js
@@ -0,0 +1,24 @@
+function removeUser() {}
+
+function createPost() {}
+
+function addItem() {}
+
+function readFile() {}
+
+function getElements() {}
+
+function createParentElement() {}
+
+function startGame() {}
+
+function sendMessage() {}
+
+removeUser();
+createPost();
+addItem();
+readFile();
+getElements();
+createParentElement();
+startGame();
+sendMessage();
