2e3a7d740b8c0f65a704ea5eeedc27bcc8b4ba88
Vakha <gvardee95@gmail.com>
2021-09-22 11:05:15 +0300

    task 2 completed

--- functions.js
+++ functions.js
@@ -22,3 +22,33 @@ getElements();
 createParentElement();
 startGame();
 sendMessage();
+
+// Функция для добавления времени
+function addUser(userData) {}
+
+// Функция возведения в степень
+function myPow(a, b) {}
+
+// Функция сложения двух чисел
+function plus(x, y) {}
+
+// Функция деления
+function division(a, b) {}
+
+// Функция умножения
+function multiplication(a, b) {}
+
+// функция вычитания
+function minus(userData) {}
+
+// Функция для вывода имени
+function addUser(name) {}
+
+// Функция для вывода возраста
+function addUser(age) {}
+
+// Функция для вывода веса
+function addUser(weight) {}
+
+// Функция для вывода цвета глащз
+function addUser(colorOfEye) {}
